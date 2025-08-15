c9adfbc8e9ad2f30783945b7ec169af94e6644b9
David Weber <david.weber.schenker@gmail.com>
2025-08-15 08:46:41 +0200

    feat: update backstage to 1.41.1

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,7 +1,7 @@
 import '@backstage/cli/asset-types';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '@backstage/canon/css/styles.css';
+import '@backstage/ui/css/styles.css';
 
 // @ts-ignore
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
